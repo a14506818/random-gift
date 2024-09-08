69f7c8a210fecd6bd131c2c7fdb7656c667ef9b0
appleSheep <a14506819@gmail.com>
2024-09-08 17:50:00 +0800

    emailJS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { storage, database } from "./firebase";
-import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
-import { ref as DBref, set, push, onValue } from "firebase/database";
+import {
+  ref,
+  uploadBytes,
+  getDownloadURL,
+  deleteObject,
+  listAll,
+} from "firebase/storage";
+import { ref as DBref, set, push, onValue, remove } from "firebase/database";
 import { v4 } from "uuid";
+import emailjs from "emailjs-com";
 
 function App() {
   const [email, setEmail] = useState("");
@@ -11,13 +18,19 @@ function App() {
   const [users, setUsers] = useState([]);
   const maxUsers = 8;
 
-  const uploadData = () => {
+  const uploadData = (e) => {
+    e.preventDefault();
+
     if (email == "") {
       alert("Enter your Email!!");
       return;
     }
     if (imgFile == null) {
       alert("Choose an Image!!");
+      return;
+    }
+    if (users.length == maxUsers) {
+      alert("No more user!!");
       return;
     }
 
@@ -32,6 +45,7 @@ function App() {
         })
           .then(() => {
             alert("Data saved successfully!");
+            window.location.reload();
           })
           .catch((error) => {
             alert("Error saving data:", error);
@@ -47,6 +61,10 @@ function App() {
     //   alert("Wait for all Users to upload!!");
     //   return;
     // }
+
+    // delete old data
+    const deleteRef = DBref(database, "sendList/");
+    remove(deleteRef);
 
     var URLList = [];
     users.forEach((user) => {
@@ -69,8 +87,43 @@ function App() {
         email: user.email,
         sendURL: randomURL,
       });
+
+      sendEmail(user.email, randomURL);
     });
-    alert("SendList saved successfully!");
+    alert("Email sended successfully!");
+  };
+
+  const sendEmail = (toMail, URL) => {
+    const templateParams = {
+      TO_MAIL: toMail,
+      message: URL,
+    };
+
+    emailjs
+      .send(
+        "service_6r7g7ym", // Replace with your EmailJS service ID
+        "template_2gabzlg", // Replace with your EmailJS template ID
+        templateParams,
+        "CSmDJXkW8dqI8aOFR"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  const resetAllData = () => {
+    const deleteRef = DBref(database, "sendList/");
+    remove(deleteRef).then(() => {
+      const usersRef = DBref(database, "users/");
+      remove(usersRef).then(() => {
+        alert("Reset success");
+      });
+    });
   };
 
   useEffect(() => {
@@ -100,24 +153,31 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Hello World!</h1>
-      <input
-        type="email"
-        onChange={(e) => {
-          setEmail(e.target.value);
-        }}
-      ></input>
-      <input
-        type="file"
-        onChange={(e) => {
-          setImgFile(e.target.files[0]);
-        }}
-      ></input>
-      <button onClick={uploadData}>Upload to DB</button>
+      <h1>2024 Farm73 最大的聖誕交換禮物抽獎平台 上線咯！</h1>
+      <form onSubmit={uploadData}>
+        <h4>Email:</h4>
+        <input
+          type="email"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        ></input>
+        <h4>Image:</h4>
+        <input
+          type="file"
+          onChange={(e) => {
+            setImgFile(e.target.files[0]);
+          }}
+        ></input>
+        <h4>Upload to DB:</h4>
+        <button type="submit">Submit</button>
+      </form>
       <h4>
         Uploaded Users ({users.length} / {maxUsers})
       </h4>
-      <button onClick={randomGifts}>Run Random Gifts</button>
+      <button onClick={randomGifts}>Run Random Gifts & Send Email</button>
+      <br></br>
+      <button onClick={resetAllData}>Reset</button>
     </div>
   );
 }
